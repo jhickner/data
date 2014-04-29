60803ace4dfec74c18e5373b89a82140155d8b5a
Jason Hickner <jhickner@gmail.com>
2014-04-29 16:55:12 -0700

    insertions

--- Data.hs
+++ Data.hs
@@ -1,38 +1,44 @@
--- CURRENT_TIMESTAMP
+{- TODO
+[ ] Data should include [String] for tags and the query needs to return them
+
+-}
+
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 import Control.Applicative
 import Control.Exception as E
 import Control.Monad
 import Data.List (intercalate)
 import Data.Monoid ((<>))
-import Data.Time.Clock
 import qualified Data.Text as T
+import Data.Time.Clock
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 
 data Data = Data Int UTCTime String deriving (Show)
-
 instance FromRow Data where
   fromRow = Data <$> field <*> field <*> field
 
+data Tag = Tag String deriving (Show)
+instance FromRow Tag where
+  fromRow = Tag <$> field
+
 main :: IO ()
 main = withConnection "data.db" $ \conn ->
-    withTransaction conn (queryAllTags ["red", "yellow"] >=> mapM_ print)
+    --withTransaction conn (queryAllTags conn ["red", "yellow"]) >>= mapM_ print
+    withTransaction conn (queryAll conn) >>= mapM_ print
 
 enableForeignKeySupport :: Connection -> IO ()
 enableForeignKeySupport = flip execute_ "PRAGMA foreign_keys = ON"
 
 queryAll :: Connection -> IO [Data]
-queryAll conn = do
-  query_ conn "SELECT id,ts,body FROM data"
+queryAll conn = query_ conn "SELECT * FROM data"
 
-queryAllTags :: [String] -> Connection -> IO [Data]
-queryAllTags tags conn = query conn sql tags
+queryAllTags :: Connection -> [String] -> IO [Data]
+queryAllTags conn tags = query conn sql tags
   where
     sql = Query $ T.concat
-      [ "SELECT d.* FROM data AS d ",
+      [ "SELECT d.* FROM data AS d "
       , "INNER JOIN tag_link as t "
       , "ON t.data_id = d.id "
       , "AND t.tag_id IN "
@@ -40,21 +46,38 @@ queryAllTags tags conn = query conn sql tags
       , "GROUP BY d.id "
       , "HAVING COUNT(d.id) = " <> count
       ]
-    -- sqlite can't parameter bind a list for the IN clause, so we
-    -- generate the correct number of ?'s in order to use scalar binding
+    -- sqlite can't parameter bind the (?,?,...) list for the IN clause, so we
+    -- generate the correct number of ?'s and then bind a [String]
     len = length tags
     count = T.pack . show $ len
     qMarks = T.intercalate "," $ replicate len "?"
 
+-- | Inserts a `Data` item along with all tags and tag_links.
+-- Should be run inside a transaction!
+insertData :: Connection -> [String] -> String -> IO Int
+insertData conn tags body = do
+    -- insert the Data and get the id
+    execute conn "INSERT INTO data(body) VALUES(?)" (Only body)
+    id' <- fromIntegral <$> lastInsertRowId conn
 
-withTransaction :: Connection -> (Connection -> IO a) -> IO a
-withTransaction conn act =
-    E.mask $ \restore -> do
-        begin
-        r <- restore (act conn) `E.onException` rollback
-        commit
-        return r
-  where
-    begin = execute_ conn "BEGIN TRANSACTION"
-    commit = execute_ conn "COMMIT TRANSACTION"
-    rollback = execute_ conn "ROLLBACK TRANSACTION"
+    -- get or create all the tags
+    tagIds <- mapM (getOrInsertTag conn) tags
+
+    -- insert tag_links for each data_id/tag_id combo
+    mapM_ (insertTagLink conn id') tagIds
+
+    return id'
+
+getOrInsertTag :: Connection -> String -> IO Int
+getOrInsertTag conn tag = do
+    res <- query conn "SELECT id FROM tag WHERE name=?" (Only tag)
+    case res of
+      (Only id':_) -> return id'
+      [] -> do
+          execute conn "INSERT INTO tag(name) VALUES(?)" (Only tag)
+          fromIntegral <$> lastInsertRowId conn
+
+insertTagLink :: Connection -> Int -> Int -> IO ()
+insertTagLink conn dataId tagId = execute conn
+    "INSERT INTO tag_link(data_id,tag_id) VALUES(?,?)"
+    [dataId, tagId]
